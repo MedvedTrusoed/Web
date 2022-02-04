8261d0a9261fc80ac76be196b6454a90c05b0b1a
Gospoja <irina.2011.14@mail.ru>
2022-02-04 18:25:29 +0300

    timer add

--- Food/dist/js/script.js
+++ Food/dist/js/script.js
@@ -1,36 +1,38 @@
-window.addEventListener('DOMContentLoaded',()=>{
+window.addEventListener('DOMContentLoaded', () => {
+
+    //Tabs
 
     const tabContent = document.querySelectorAll('.tabcontent'),
-          tabheaderItem = document.querySelectorAll('.tabheader__item'),
-          tabheaderItemsParent = document.querySelector('.tabheader__items');
+        tabheaderItem = document.querySelectorAll('.tabheader__item'),
+        tabheaderItemsParent = document.querySelector('.tabheader__items');
 
-    function hideTabsContent(){
+    function hideTabsContent() {
 
         tabContent.forEach(item => {
             item.style.display = 'none';
         });
 
-        tabheaderItem.forEach(item=>{
+        tabheaderItem.forEach(item => {
             item.classList.remove('tabheader__item_active');
         });
     };
 
-    function showTabContent(i=0){
+    function showTabContent(i = 0) {
         tabContent[i].style.display = 'block';
         tabheaderItem[i].classList.add('tabheader__item_active');
-        
+
     }
 
     hideTabsContent();
     showTabContent();
 
-    tabheaderItemsParent.addEventListener('click',(event)=>{
+    tabheaderItemsParent.addEventListener('click', (event) => {
 
         const target = event.target;
 
-        if(target && target.classList.contains('tabheader__item')){
-            tabheaderItem.forEach((item,index)=>{
-                if(target==item){
+        if (target && target.classList.contains('tabheader__item')) {
+            tabheaderItem.forEach((item, index) => {
+                if (target == item) {
                     hideTabsContent();
                     showTabContent(index);
                 }
@@ -38,4 +40,75 @@ window.addEventListener('DOMContentLoaded',()=>{
 
         }
     });
+
+    //Timer
+
+    const deadline = '2022-02-15';
+
+    function timeToEnd(endTime) {
+
+        const t = Date.parse(endTime) - Date.parse(new Date()),
+            days = Math.floor((t / (1000 * 60 * 60 * 24))),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / (1000 * 60) % 60)),
+            seconds = Math.floor((t / 1000) % 60);
+
+        console.log(seconds);
+        console.log(minutes);
+        console.log(hours);
+        console.log(days);
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+
+    }
+
+    function zeroTime(num) {
+
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setTimer(selector, endTime) {
+
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateTimer, 1000);
+
+        updateTimer();
+
+        function updateTimer() {
+
+            let time = timeToEnd(endTime);
+
+            days.innerHTML = zeroTime(time.days);
+            hours.innerHTML = zeroTime(time.hours);
+            minutes.innerHTML = zeroTime(time.minutes);
+            seconds.innerHTML = zeroTime(time.seconds);
+
+            if (time.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+
+
+    }
+
+
+    setTimer('.timer', deadline);
+
+    //    let t = timeToEnd(deadline);
+    //    console.log(t.days);
+
 });
